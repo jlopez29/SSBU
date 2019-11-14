1914f8759a0d9d56f379f4a39996fd777c285cef
jlopez <jlopez@mears.com>
2019-11-14 17:35:44 -0500

    - Added horizontal recycler for stats

--- app/src/main/java/com/jlapps/ssbu/Model/Character.kt
+++ app/src/main/java/com/jlapps/ssbu/Model/Character.kt
@@ -9,6 +9,7 @@ class Character constructor(id:Int,name:String,series:String,tier:Char,image:Int
     var mark = mark
     var isFavorite = false
     var isSelected = false
+    lateinit var attributes:CharacterAttributes
 
     constructor():this(-1,"Unselected","",'Z',-1,-1)
 }
---
+++ app/src/main/java/com/jlapps/ssbu/Model/CharacterAttributes.kt
@@ -0,0 +1,12 @@
+package com.jlapps.ssbu.Model
+
+class CharacterAttributes constructor(damage:Int,neutral:Int,recovery:Int,killPower:Int,defense:Int,speed:Int,weight:Int,taunt:Int){
+    var damage = damage
+    var neutral = neutral
+    var recovery = recovery
+    var killPower = killPower
+    var defense = defense
+    var speed = speed
+    var weight = weight
+    var taunt = taunt
+}
